1c72ec562a619b9b2de887383dd7398b94aa5f85
Olalekan <odukoyaonline@gmail.com>
2020-12-20 14:57:02 +0100

    refactor query

--- src/validation/records.js
+++ src/validation/records.js
@@ -9,7 +9,7 @@ const fetchRecordsValidator = Joi.object({
     .date()
     .format('YYYY-MM-DD')
     .required()
-    .error(new Error('Kindly enter a valid start date.')),
+    .error(new Error('Kindly enter a valid end date.')),
   minCount: Joi.number().required().error(new Error('Kindly enter a minimum amount.')),
   maxCount: Joi.number().required().error(new Error('Kindly enter a maximum amount.')),
 });
